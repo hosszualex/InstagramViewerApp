dc8875ae0e18e75f6a266b3413129c461ea7be93
Alex Hosszu <hosszualex99@gmai.com>
2022-03-10 11:25:18 +0200

    Removed sensitive information

--- app/src/main/java/com/example/instagramviewerapp/Constants.kt
+++ app/src/main/java/com/example/instagramviewerapp/Constants.kt
@@ -3,7 +3,7 @@ package com.example.instagramviewerapp
 object Constants {
     //=====API CONSTANTS=====//
     const val INSTAGRAM_API_URL = "https://graph.instagram.com/v13.0/"
-    const val ACCESS_TOKEN = "IGQVJVU2tiM2MxcjlnM2dTTk1nX3U5X0xxbXNLaURaVHptYlJ0YmpSLURGck15dC1aRUdRMlVxVENERDBsOGZAjakFJLTEwNUFOSFliOFdUWXZAPWGtiRnRleUIwSUNVaDVtM0dBNzhB"
+    const val ACCESS_TOKEN = ""
     const val GRANT_TYPE = "ig_refresh_token"
     const val POST_FIELD = "id,caption,media_type,media_url,permalink,thumbnail_url,username,timestamp,children"
     const val CHILDREN_FIELD = "id,media_type,media_url,permalink,thumbnail_url,username,timestamp"
